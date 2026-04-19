21c4e75f800138ba8ce8219f3b41f25fe39e3cd2
Nreisny <nreisny@yahoo.com>
2026-04-19 02:33:40 -0400

    Did some light trimming of previous files from setup and created a simple navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import './App.css';
 
 function App(){
+  function Home(){
+    return(
+      <h1>Home</h1>
+    );
+  }
+  function Sharding(){
+    return(
+      <h1>Sharding</h1>
+    );
+  }
   return(
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <nav>
+        <Link to='/' className='home-li active-link'>Home</Link>
+        <Link to='/Sharding'>Sharding</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Sharding' element={<Sharding />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
