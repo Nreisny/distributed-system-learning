f1db8c731889499d2d1de5989b7d11f1ced6543d
Nreisny <nreisny@yahoo.com>
2026-04-19 21:36:25 -0400

    Added some basic logic to get values from input field as well as incrementing and decrementing that value with side buttons.  Also added a bit more CSS.

--- src/Sharding.tsx
+++ src/Sharding.tsx
@@ -1,29 +1,64 @@
+import { useState } from "react";
 import "./sharding.css";
 
 function Sharding() {
+  const [numServers, setNumServers] = useState<number>(0);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setNumServers(Number(e.target.value));
+  }
+
+  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(numServers);
+  }
+
+  function onDecrementClick() {
+    setNumServers(numServers - 1);
+  }
+
+  function onIncrementClick() {
+    setNumServers(numServers + 1);
+  }
+
   return (
-    <div className="container">
-      <form className="card">
-        <h2>Sharding Setup</h2>
+    <>
+      <div className="setupContainer">
+        <form className="setupCard" onSubmit={handleSubmit}>
+          <h2>Sharding Setup</h2>
 
-        <label>Enter the number of inital servers</label>
+          <label>Enter the number of inital servers</label>
 
-        <div className="inputArea">
-          <input type="text" placeholder="3" />
+          <div className="setupInputArea">
+            <input
+              type="text"
+              placeholder="3"
+              onChange={handleChange}
+              value={numServers}
+            ></input>
 
-          <button type="button" className="inputBtn">
-            −
+            <button
+              type="button"
+              className="inputBtn"
+              onClick={onDecrementClick}
+            >
+              −
+            </button>
+            <button
+              type="button"
+              className="inputBtn"
+              onClick={onIncrementClick}
+            >
+              +
+            </button>
+          </div>
+
+          <button type="submit" className="submitBtn">
+            Submit
           </button>
-          <button type="button" className="inputBtn">
-            +
-          </button>
-        </div>
-
-        <button type="submit" className="submitBtn">
-          Submit
-        </button>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 }
 
