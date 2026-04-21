b60a0d0d176d40a162a20459fc55a7b893da955f
Nreisny <nreisny@yahoo.com>
2026-04-20 20:51:33 -0400

    Refactored my react code to allow for more reusable functions and changed the naiming of some classNames

--- src/Sharding.tsx
+++ src/Sharding.tsx
@@ -2,52 +2,104 @@ import { useState } from "react";
 import "./sharding.css";
 
 function Sharding() {
-  const [numServers, setNumServers] = useState<number>(0);
+  type Form = {
+    numServers: number;
+    numRepeats: number;
+  };
 
+  const [formData, setFormData] = useState<Form>({
+    numServers: 0,
+    numRepeats: 0,
+  });
+
+  // Updates the value of the field that is given in the name
+  // of the HTML input element
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setNumServers(Number(e.target.value));
+    setFormData((prevalue) => {
+      return {
+        ...prevalue,
+        [e.target.name]: Number(e.target.value),
+      };
+    });
   }
 
+  // Currently just shows all the values in console, will be imporved
+  // later to send data to actually initalize the setup
   function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log(numServers);
   }
 
-  function onDecrementClick() {
-    setNumServers(numServers - 1);
+  // Decrements the value of the values name that is passed through
+  function onDecrementClick(valueName: keyof Form) {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [valueName]: prev[valueName] - 1,
+      };
+    });
   }
 
-  function onIncrementClick() {
-    setNumServers(numServers + 1);
+  function onIncrementClick(valueName: keyof Form) {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [valueName]: prev[valueName] + 1,
+      };
+    });
   }
 
   return (
     <>
-      <div className="setupContainer">
-        <form className="setupCard" onSubmit={handleSubmit}>
+      <div className="container">
+        <form className="card" onSubmit={handleSubmit}>
           <h2>Sharding Setup</h2>
 
           <label>Enter the number of inital servers</label>
 
-          <div className="setupInputArea">
+          <div className="inputArea">
             <input
               type="text"
               placeholder="3"
               onChange={handleChange}
-              value={numServers}
+              value={formData.numServers}
+              name="numServer"
             ></input>
 
             <button
               type="button"
               className="inputBtn"
-              onClick={onDecrementClick}
+              onClick={() => onDecrementClick("numServers")}
             >
               −
             </button>
             <button
               type="button"
               className="inputBtn"
-              onClick={onIncrementClick}
+              onClick={() => onIncrementClick("numServers")}
+            >
+              +
+            </button>
+          </div>
+          <label>Enter the number of repeated servers</label>
+          <div className="inputArea">
+            <input
+              type="text"
+              placeholder="3"
+              onChange={handleChange}
+              value={formData.numRepeats}
+            ></input>
+
+            <button
+              type="button"
+              className="inputBtn"
+              onClick={() => onDecrementClick("numRepeats")}
+            >
+              −
+            </button>
+            <button
+              type="button"
+              className="inputBtn"
+              onClick={() => onIncrementClick("numRepeats")}
             >
               +
             </button>
@@ -57,6 +109,16 @@ function Sharding() {
             Submit
           </button>
         </form>
+      </div>
+      <div>
+        <div className="container">
+          <form className="card">
+            <label>Enter a key</label>
+            <div className="inputArea">
+              <input type="text" placeholder="banana"></input>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
